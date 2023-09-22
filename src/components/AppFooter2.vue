40c0a9195382f992c309f1517327e1df490dffc9
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-22 20:27:30 +0200

    add footer 2

--- src/components/AppFooter2.vue
+++ src/components/AppFooter2.vue
@@ -13,7 +13,41 @@ export default {
 
 <template>
 
+<footer>
+        <div class="container w-75">
+            <div class="row">
+                <div class="col-12 d-flex align-items-center justify-content-between">
+                    <div class="bottone">
+                        <button>SIGN-UP-NOW!</button>
+                    </div>
+
+                    <div class="d-flex align-items-center">
+                        <h3>FOLLOW US</h3>
+                        <img src="../assets/img/footer-facebook.png" alt="">
+                        <img src="../assets/img/footer-twitter.png" alt="">
+                        <img src="../assets/img/footer-youtube.png" alt="">
+                        <img src="../assets/img/footer-pinterest.png" alt="">
+                        <img src="../assets/img/footer-periscope.png" alt="">
+
+                    </div>
+
+
+                </div>
+                
+            </div>
+        </div>
+    </footer>
+
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+footer{
+    background-color: #303030;
+}
+h3{
+    color: #0282f9;
+}
+
+</style>
