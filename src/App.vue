4f94c0818fd49464071e571d47bdf3e398a033f7
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-22 15:55:17 +0200

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <script>
+import AppFooter1 from './components/AppFooter1.vue';
+import AppFooter2 from './components/AppFooter2.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppJumbotron from './components/AppJumbotron.vue';
+import AppMainContent from './components/AppMainContent.vue';
+
+export default{
+  data(){
+    return{
+
+    }
+  },
+  components: {
+    AppHeader,
+    AppJumbotron,
+    AppMainContent,
+    AppFooter1,
+    AppFooter2,
+  }
+}
 
 </script>
 
 
 
 <template>
+
+  <AppHeader></AppHeader>
  
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,3 +3,13 @@ import './assets/scss/app.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+/* 
+Descrizione:
+Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
+
+Bonus:
+Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
+*/
