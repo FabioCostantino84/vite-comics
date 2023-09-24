6403b73d145b91f87b9053cb90691155491f377c
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-24 18:44:18 +0200

    add fixed logo brands

--- src/components/AppFooter2.vue
+++ src/components/AppFooter2.vue
@@ -4,6 +4,29 @@ export default {
     name: "AppFooter2",
     data() {
         return {
+
+            brands: [
+                {
+                    name: 'facebook',
+                    logo: 'src/assets/img/footer-facebook.png'
+                },
+                {
+                    name: 'twitter',
+                    logo: 'src/assets/img/footer-twitter.png'
+                },
+                {
+                    name: 'youtube',
+                    logo: 'src/assets/img/footer-youtube.png'
+                },
+                {
+                    name: 'pinterest',
+                    logo: 'src/assets/img/footer-pinterest.png'
+                },
+                {
+                    name: 'periscope',
+                    logo: 'src/assets/img/footer-periscope.png'
+                },
+            ]
         }
     }
 }
@@ -12,8 +35,7 @@ export default {
 
 
 <template>
-
-<footer>
+    <footer>
         <div class="container w-75">
             <div class="row">
                 <div class="col-12 d-flex align-items-center justify-content-between">
@@ -21,33 +43,36 @@ export default {
                         <button>SIGN-UP-NOW!</button>
                     </div>
 
-                    <div class="d-flex align-items-center">
-                        <h3>FOLLOW US</h3>
-                        <img src="../assets/img/footer-facebook.png" alt="">
-                        <img src="../assets/img/footer-twitter.png" alt="">
-                        <img src="../assets/img/footer-youtube.png" alt="">
-                        <img src="../assets/img/footer-pinterest.png" alt="">
-                        <img src="../assets/img/footer-periscope.png" alt="">
+                    <div class="d-flex align-items-center p-5">
+                        <div>
+                            <h4>FOLLOW US</h4>
+                        </div>
+                        <div v-for="brand in brands" class="iconsBrands d-flex flex-wrap p-2 align-items-center">
+                            <img :src="brand.logo" :alt="brand.name">
+
+
+                        </div>
 
                     </div>
 
 
                 </div>
-                
+
             </div>
         </div>
     </footer>
-
 </template>
 
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
 
-footer{
+footer {
     background-color: #303030;
 }
-h3{
-    color: #0282f9;
-}
 
+h4 {
+    color: $primary-color;
+    padding: 1rem;
+}
 </style>
