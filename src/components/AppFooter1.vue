c857f8dc3b26f10ff30986e2049a3eb41b930c60
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-24 17:54:46 +0200

    add fixed footer1 container

--- src/components/AppFooter1.vue
+++ src/components/AppFooter1.vue
@@ -14,55 +14,60 @@ export default {
 <template>
 
     <div class="f_container">
-        <div class="w-75 m-auto row">
-            <div class=" d-flex col-6 mt-4">
-               
-                    <div class="d-flex flex-column">
-                        <h3>DC COMICS</h3>
-                        <a href="">Characters</a>
-                        <a href="">Comics</a>
-                        <a href="">Movies</a>
-                        <a href="">TV</a>
-                        <a href="">Games</a>
-                        <a href="">Videos</a>
-                        <a href="">News</a>
 
-                        <h3>SHOP</h3>
-                        <a href="">Shop DC</a>
-                        <a href="">Shop DC Collectibles</a>
+        <div class="container w-75 m-auto">
+
+            <div class="row">
+                <div class="d-flex col-6 mt-4">
+                   
+                        <div class="d-flex flex-column">
+                            <h3>DC COMICS</h3>
+                            <a href="">Characters</a>
+                            <a href="">Comics</a>
+                            <a href="">Movies</a>
+                            <a href="">TV</a>
+                            <a href="">Games</a>
+                            <a href="">Videos</a>
+                            <a href="">News</a>
+    
+                            <h3>SHOP</h3>
+                            <a href="">Shop DC</a>
+                            <a href="">Shop DC Collectibles</a>
+                        </div>
+                        <div class="d-flex flex-column ps-5">
+                            <h3 class="">DC</h3>
+                            <a href="">Terms Of Use</a>
+                            <a href="">Privacy Policy (New)</a>
+                            <a href="">Add Choices</a>
+                            <a href="">Advertising</a>
+                            <a href="">Jobs</a>
+                            <a href="">Subscription</a>
+                            <a href="">Talent Workshops</a>
+                            <a href="">CPSC Certificates</a>
+                            <a href="">Ratings</a>
+                            <a href="">Shop Help</a>
+                            <a href="">Contact Us</a>
+                        </div>
+                        <div class="d-flex flex-column ps-5">
+                            <h3>SITES</h3>
+                            <a href="">DC</a>
+                            <a href="">MAD Magazine</a>
+                            <a href="">DC Kids</a>
+                            <a href="">DC Universe</a>
+                            <a href="">DC Power Visa</a>
+                        </div>
+    
+                </div>
+    
+                <div class="col-6">
+                    <div class="box">
+                        
+                        <img src="../assets/img/dc-logo-bg.png" alt="">
+    
                     </div>
-                    <div class="d-flex flex-column ps-5">
-                        <h3 class="">DC</h3>
-                        <a href="">Terms Of Use</a>
-                        <a href="">Privacy Policy (New)</a>
-                        <a href="">Add Choices</a>
-                        <a href="">Advertising</a>
-                        <a href="">Jobs</a>
-                        <a href="">Subscription</a>
-                        <a href="">Talent Workshops</a>
-                        <a href="">CPSC Certificates</a>
-                        <a href="">Ratings</a>
-                        <a href="">Shop Help</a>
-                        <a href="">Contact Us</a>
-                    </div>
-                    <div class="d-flex flex-column ps-5">
-                        <h3>SITES</h3>
-                        <a href="">DC</a>
-                        <a href="">MAD Magazine</a>
-                        <a href="">DC Kids</a>
-                        <a href="">DC Universe</a>
-                        <a href="">DC Power Visa</a>
-                    </div>
-
-            </div>
-
-            <div class="col-6">
-                <div class="box">
-                    
-                    <img src="../assets/img/dc-logo-bg.png" alt="">
-
                 </div>
             </div>
+
         </div>
 
     </div>
@@ -87,15 +92,6 @@ export default {
     width: 500px;
     margin-top: -85px;
 }
-
-/* .box{
-    width: 300px;
-}
-
-.box > img{
-    width: 100%;
-    height: auto;
-} */
 
 h3 {
     color: #ffffff;
