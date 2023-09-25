e9fb9f776ebdde51dc74eccd344615708432f5b8
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-25 13:27:49 +0200

    add img cards

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -1,10 +1,12 @@
 <script>
+import cards from '../card.js'
 export default {
 
     name: "AppJumbotron",
 
     data() {
         return {
+            cards,
         }
     }
 }
@@ -15,12 +17,23 @@ export default {
 <template>
     <div class="jumbotron">
 
-        <div class="container w-75 m-auto align-items-md-stretch">
+        <div class="container text-center ">
+            <div class="row row-cols-6">
+                <div v-for="card in cards.cards" class="card flex-nowrap bg-black">
+                    <img :src="card.thumb" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-text text-white">{{ card.series }}</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- <div class="container w-75 m-auto align-items-md-stretch">
 
             <div class="py-5 text-white">
                 <h2>Content goes here</h2>
             </div>
-        </div>
+        </div> -->
 
     </div>
 </template>
@@ -31,5 +44,10 @@ export default {
 
 .jumbotron {
     background-color: $black-color;
+}
+.card>img{
+    width: 100%;
+
+    aspect-ratio: 1/1;
 }
 </style>
